50e56f20cc4a324b6d662d3a051de8aef257aaf4
Timothy Jones <timothy.l.jones@gmail.com>
2022-04-28 17:25:22 +1000

    Remove transactional annotation since it is now covered by the repository

--- user-management/src/main/java/com/sivalabs/usermanagement/domain/registration/UserRegistrationService.java
+++ user-management/src/main/java/com/sivalabs/usermanagement/domain/registration/UserRegistrationService.java
@@ -6,10 +6,8 @@ import com.sivalabs.usermanagement.domain.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 @Slf4j
 public class UserRegistrationService {
